5e392c0c93c3f3b62d432b12eec26dc2f1935c3e
atul <atul.etech2011@gmail.com>
2024-02-01 16:06:36 +0530

    Add Cors

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import express from "express";
+import cors from "cors";
 import connectDB from "./config/database";
 import apiRoutes from "./routes/api";
 
 const app = express();
 app.use(express.json());
+
+app.use(cors());
 
 connectDB();
 
