71a07a786132a4baa6b7081a95903353cbea74b5
atul <atul.etech2011@gmail.com>
2024-02-09 17:21:07 +0530

    update profile api added

--- src/routes/signin.routes.ts
+++ src/routes/signin.routes.ts
@@ -1,8 +1,12 @@
 import express from "express";
-import { signIn } from "../controllers/signin.controller";
+import { getAllCtrl, loginCtrl } from "../controllers/signin.controller";
+// import { signIn } from "../controllers/signin.controller";
 
 const router = express.Router();
 
-router.post("/signIn", signIn);
+// router.post("/signIn", signIn);
+router.post("/signIn", loginCtrl);
+router.get("/get-user", getAllCtrl);
+
 
 export default router;
